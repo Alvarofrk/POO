35ae435c4ae7d6d6f7975e9825126d1d44d259af
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-28 02:50:09 -0500

    Create clase.cpp

---
+++ clase.cpp
@@ -0,0 +1,11 @@
+class pareja {      
+   public:
+	// Datos miembro de la clase "pareja"
+      int a, b; 
+
+      // Funciones miembro de la clase "pareja"
+      void Lee (int _a, int _b) {
+         a = a2;
+         b = b2;
+      }
+};
